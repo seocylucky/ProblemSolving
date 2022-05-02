29f79b2c55cc7f972dfdfcb313f34b74f67c7689
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2022-05-02 17:40:44 +0900

    algo upload

---
+++ 기초100제/산술연산/basic39.py
@@ -0,0 +1,6 @@
+#실수 2개 입력받아 거듭제곱 계산하기
+
+a, b = input().split()
+a = float(a)
+b = float(b)
+print(a**b)
---
+++ 기초100제/산술연산/basic43.py
@@ -0,0 +1,7 @@
+#실수 2개 입력받아 나눈 결과 계산하기
+
+a, b = input().split()
+a = float(a)
+b = float(b)
+c = a/b 
+print(format(c,".3f"))
