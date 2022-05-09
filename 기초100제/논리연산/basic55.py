511480e782a378ec5ea0629c9cfb96f3f69b44ec
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2022-05-09 23:55:04 +0900

    algo upload

--- 기초100제/논리연산/basic55.py
+++ 기초100제/논리연산/basic55.py
@@ -1,4 +1,5 @@
-#하나라도 참이면 참 출력하기
+#참/거짓이 서로 다를 때에만 참 출력하기
 
 a, b = input().split()
+
 print(bool(int(a)) or bool(int(b)))
--- 기초100제/논리연산/basic56.py
+++ 기초100제/논리연산/basic56.py
@@ -1,5 +1,6 @@
 #참/거짓이 서로 다를 때에만 참 출력하기
 
-a, b = input().split()
-
-print(bool(int(a)) or bool(int(b)))
+a, b=input().split()
+c = bool(int(a))
+d = bool(int(b))
+print((c and (not d)) or ((not c) and d))
