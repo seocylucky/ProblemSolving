95e73c9305b721c09f780d55bc27bc899b610335
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2022-06-24 22:35:39 +0900

    algo upload

---
+++ 기초100제/조건_선택실행구조/basic69.py
@@ -0,0 +1,14 @@
+# 평가 입력받아 다르게 출력하기
+
+score = str(input())
+
+if score == 'A' :
+    print('best!!!')
+elif score == 'B' :
+    print('good!!')
+elif score == 'C' :
+    print('run!')
+elif score == 'D' :
+    print('slowly~')
+else :
+    print('what?')
