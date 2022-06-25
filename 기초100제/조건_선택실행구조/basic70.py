f7a45552b4ba3c5df47d258d2d76f32371793362
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2022-06-25 23:46:32 +0900

    algo upload

---
+++ 기초100제/조건_선택실행구조/basic70.py
@@ -0,0 +1,12 @@
+# 월 입력받아 계절 출력하기
+
+month = int(input())
+
+if month // 3 == 1 :
+    print('spring')
+elif month // 3 == 2 :
+    print('summer')
+elif month // 3 == 3 :
+    print('fall')
+else :
+    print('winter')
