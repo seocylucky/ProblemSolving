2689ee9ac30f3dd614577206a602314590472346
seocylucky <chaeyeon2285@naver.com>
2024-01-16 22:31:54 +0900

    20240116_6044

---
+++ 기초100제/산술연산/6044.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+
+intA = int(a)
+intB = int(b)
+
+print(intA + intB)
+print(intA - intB)
+print(intA * intB)
+print(intA // intB)
+print(intA % intB)
+print(format(intA / intB, ".2f"))
