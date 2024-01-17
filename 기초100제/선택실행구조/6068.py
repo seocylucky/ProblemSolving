386f60ea9bec0e02b344b421e2d8f0519c6eee3e
seocylucky <chaeyeon2285@naver.com>
2024-01-17 18:29:39 +0900

    20240117_6068

---
+++ 기초100제/선택실행구조/6068.py
@@ -0,0 +1,11 @@
+score = int(input())
+
+if 0 <= score <= 100 :
+    if 90 <= score <= 100 :
+        print("A")
+    elif 70 <= score <= 89 :
+        print("B")
+    elif 40 <= score <= 69 :
+        print("C")
+    else:
+        print("D")
