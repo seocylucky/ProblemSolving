f2cc4392fb8873026b503f40db2ad5ae611b8c5b
seocylucky <chaeyeon2285@naver.com>
2024-09-13 15:08:11 +0900

    BOJ-EX: 2024. 9. 13. 오후 3:08:10

--- 2473번： 세 용액/세 용액.py
+++ 2473번： 세 용액/세 용액.py
@@ -2,23 +2,30 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().split()))
-sorted(arr)
+arr = sorted(arr)
 
 resultArr = []
 
 start = 0
-end = N-1
-mid = start + 1
 minSum = 3000000000
 curSum = 0
 
 for start in range(N-2):
+  end = N-1
+  mid = start + 1
+
   while mid < end : 
     curSum = arr[start] + arr[mid] + arr[end]
 
-  if curSum < minSum :
-    minSum = curSum
-    resultArr = [arr[start], arr[mid], arr[end]]
+    if abs(curSum) < abs(minSum) :
+      minSum = curSum
+      resultArr = [arr[start], arr[mid], arr[end]]
+
+    if curSum < 0:
+        mid += 1
+    else:
+        end -= 1
 
 
-print(sorted(resultArr))
+for i in resultArr:
+  print(i, end=' ')
