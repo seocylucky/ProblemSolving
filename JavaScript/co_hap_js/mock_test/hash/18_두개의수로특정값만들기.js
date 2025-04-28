8401f1aa8e1ae6efad48f41ae7a6ba830daa0afa
seocylucky <chaeyeon2285@naver.com>
2025-04-29 00:52:33 +0900

    BOJ-EX: 2025. 4. 29. 오전 12:52:32

--- JavaScript/co_hap_js/mock_test/hash/18_두개의수로특정값만들기.js
+++ JavaScript/co_hap_js/mock_test/hash/18_두개의수로특정값만들기.js
@@ -1,6 +1,27 @@
-function solution(arr, target) {
-  let remains = arr.map((num) => target - num);
-  return remains.includes(target);
+function countSort(arr, target) {
+  const hashtable = Array.from({ length: target + 1 }).fill(0);
+
+  for (const num of arr) {
+    if (num <= target) {
+      hashtable[num] = 1;
+    }
+  }
+
+  return hashtable;
 }
 
-console.log(solution([1, 2, 3, 4, 8], 6));
+function solution(arr, target) {
+  const hashtable = countSort(arr, target);
+
+  for (const num of arr) {
+    const remains = target - num;
+
+    if (remains !== num && remains <= target && hashtable[remains] === 1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(solution([2, 3, 5, 9], 10));
